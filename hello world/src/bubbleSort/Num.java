da620988ed9da2cad432a9104b4c597728a38a22
raymondhe <372473552@qq.com>
2019-02-12 07:04:14 +0800

    My Job
    SortMachine

---
+++ hello world/src/bubbleSort/Num.java
@@ -0,0 +1,27 @@
+package bubbleSort;
+
+public class Num {
+
+
+public Num(int n) {
+		super();
+		this.n = n;
+	}
+
+public int getN() {
+	return n;
+}
+public void setN(int n) {
+	this.n = n;
+}
+
+
+@Override
+public String toString() {
+	return "Num [n=" + n + "]";
+}
+
+
+private int n;
+
+}
